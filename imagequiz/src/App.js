154b4ae2ece22a806eb72c33f6b351102e2370c9
Jiazheng <jasonhjz9527@gmail.com>
2021-03-10 21:44:04 -0800

    Update

--- imagequiz/src/App.js
+++ imagequiz/src/App.js
@@ -1,10 +1,24 @@
-import "./App.css";
+import { HashRouter, Switch, Route } from "react-router-dom";
+import React from "react";
+
+import Header from "./components/Header.js";
+import Footer from "./components/Footer.js";
+import Navbar from "./components/Navbar.js";
+
+import Home from "./components/Home.js";
+import Login from "./components/Login.js";
 
 function App() {
     return (
-        <div className="App">
-            <h1>Hello World!!!</h1>
-        </div>
+        <HashRouter>
+            <Header />
+            <Navbar />
+            <Switch>
+                <Route path="/home" component={Home} />
+                <Route path="/login" component={Login} />
+            </Switch>
+            <Footer />
+        </HashRouter>
     );
 }
 
